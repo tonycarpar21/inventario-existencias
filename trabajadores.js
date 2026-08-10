712564286e90580fd05a709338357dbed4a7807e
tonycarpar21 <tonycarpar21@gmail.com>
2026-08-09 20:18:07 -0500

    Add files via upload

---
+++ trabajadores.js
@@ -0,0 +1,32 @@
+/* =====================================
+   ARCHIVO GENERADO AUTOMÁTICAMENTE
+   NO MODIFICAR MANUALMENTE
+===================================== */
+
+window.datosEmpresa = {
+  nombre: "Almacén XXXXXX",
+  ruc: "0909090909090909"
+};
+
+window.trabajadores = [
+  {
+    nombre: "Carranza Paredes, Anthony David",
+    dni: "72420939"
+  },
+  {
+    nombre: "Carranza Paredes, Aldo Jimmy",
+    dni: "72420936"
+  },
+  {
+    nombre: "Carranza Paredes, Arnold Piero",
+    dni: "72420940"
+  },
+  {
+    nombre: "Carranza Lopez, Arnold Jimmy",
+    dni: "07634877"
+  },
+  {
+    nombre: "Lopez Cordova, Kenia Dennis",
+    dni: "75192933"
+  },
+];
